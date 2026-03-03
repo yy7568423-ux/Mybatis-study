3f8685c4562176c95406534ac7a0ebe45430ec0d
DDDJ <2811723217@qq.com>
2026-03-03 10:38:54 +0800

    日志工厂

--- mybatis-03/src/test/java/dao/UserMapperTest.java
+++ mybatis-03/src/test/java/dao/UserMapperTest.java
@@ -4,6 +4,7 @@ import com.dddj.dao.UserMapper;
 import com.dddj.pojo.User;
 import com.dddj.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import java.util.List;
@@ -12,14 +13,23 @@ import java.util.List;
  * @author DDDJ
  **/
 public class UserMapperTest {
+
+    static Logger logger =Logger.getLogger(UserMapperTest.class);
     @Test
     public void getUserList(){
         SqlSession sqlSession = MybatisUtils.getSqlSession();
+        logger.info("i测试:进入方法");
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
         List<User> userList = userMapper.getUserList();
         for (User user : userList) {
             System.out.println(user);
         }
         sqlSession.close();
+    }
+
+    //简单引用log4j
+    @Test
+    public void testLog4j(){
+
     }
    }
